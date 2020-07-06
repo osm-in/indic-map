e18a017d15eeee2146e5011f6beebe5de7626fbe
planemad <arun.planemad@gmail.com>
2020-07-06 01:23:27 -0400

    Improve querying by paging

--- data/join-wikidata-labels.js
+++ data/join-wikidata-labels.js
@@ -8,12 +8,29 @@
 //
 // Add localised labels from Wikidata to the GeoJSON data
 //
-// 1. Export OSM data as a GeoJSON. 
+// 1. Export OSM data as export.geojson
+// - world-places http://overpass-turbo.eu/s/VPm
+// - india-urban-places http://overpass-turbo.eu/s/VPn
+// - india-rural-places http://overpass-turbo.eu/s/VPn
+// 2. Run `./join-wikidata-labels.js`
+// 3. `tippecanoe -zg -o indic-places.mbtiles --drop-densest-as-needed --extend-zooms-if-still-dropping world-places-translated.geojson india-urban-places-translated.geojson india-rural--places-translated.geojson`
 //
 
 const fs = require('fs');
-const fetch = require('node-fetch');
+
+const nodeFetch = require('node-fetch');
+var fetch = require('fetch-retry')(nodeFetch, {
+    retries: 5,
+    retryDelay: 500
+});
+
 const _ = require('lodash');
+const Bottleneck = require("bottleneck/es5");
+
+const limiter = new Bottleneck({
+    minTime: 50,
+    maxConcurrent: 5
+});
 
 // const WDK = require('wikibase-sdk');
 
@@ -23,33 +40,37 @@ const _ = require('lodash');
 // })
 
 // let geojson = fs.readFileSync('places.geojson');
-let geojson = fs.readFileSync('places2.geojson');
-let features = JSON.parse(geojson).features;
+
+const inputFileName = 'india-rural-places.geojson'
+const outputFileName = inputFileName.split('.')[0] + '-translated.geojson'
+
+const features = JSON.parse(fs.readFileSync(inputFileName)).features;
 
 
-let languages = ["as", "bn", "brx", "doi", "en", "gu", "hi", "kn", "ks", "gom", "mai", "ml", "mni", "mr", "ne", "or", "pa", "sa", "sat", "sd", "ta", "te", "ur"];
-let props = ["place", "wikidata", "name"]
-languages.forEach(item=> props.push("name:"+item))
+const languages = ["as", "bn", "brx", "doi", "en", "gu", "hi", "kn", "ks", "gom", "mai", "ml", "mni", "mr", "ne", "or", "pa", "sa", "sat", "sd", "ta", "te", "ur"];
 
-let qidField = 'wikidata'
-let preferWikidataLabel = true
-let requestChunkSize = 40
+let filterProps = ["place", "wikidata", "name"]
+languages.forEach(item => filterProps.push("name:" + item))
+
+const qidField = 'wikidata'
+const preferWikidataLabel = true
+const requestChunkSize = 250
 
 
 let joinWikidataLabels = function (features, languages) {
 
     // Filter features to only include those with a qid
 
-    features = features.filter(item=>
-       item.properties.hasOwnProperty(qidField)
+    features = features.filter(item =>
+        item.properties.hasOwnProperty(qidField)
     )
-    
+
     // Drop all fields except names and wikidata id
 
-    features.map(item=>{
-        Object.keys(item.properties).forEach(prop=>{
-            
-            if (!prop.startsWith("name") && ['place','wikidata','@id'].indexOf(prop)==-1){
+    features.map(item => {
+        Object.keys(item.properties).forEach(prop => {
+
+            if (!prop.startsWith("name") && ['place', 'wikidata', '@id'].indexOf(prop) == -1) {
                 delete item.properties[prop]
             }
         })
@@ -76,13 +97,15 @@ let joinWikidataLabels = function (features, languages) {
         requests.push(translateQids(qidsPage, languages))
     }
 
+    console.log(`Fetching Wikidata translations from ${requests.length} API requests`)
+
     // Collect all the Wikidata results
 
     Promise.all(requests).then(data => {
 
-        data = Object.assign({}, ...data)
+        console.log(`Processing results`)
 
-        // console.log(data)
+        data = Object.assign({}, ...data)
 
         // Join properties from Wikidata
         // Add translation if it exists
@@ -106,25 +129,32 @@ let joinWikidataLabels = function (features, languages) {
 
             })
 
-            item.properties = _.pick(item.properties, props);
+            item.properties = _.pick(item.properties, filterProps);
 
         });
 
         let geoJSON = {
             "type": "FeatureCollection",
             "features": features
-          }
-        // console.log(JSON.stringify(geoJSON))
-        console.log(features)
-        return features;
+        }
+
+        fs.writeFileSync(outputFileName, JSON.stringify(geoJSON));
 
     });
 
 }
 
+//
+// Get the translated labels for the given Wikidata QIDs in the requested languages
+//
+
+
 
 let translateQids = function (qids, languages) {
-    let sparql = `
+
+    // Build a query for the Wikidata SPARQL endpoint
+
+    let sparqlQuery = `
         # Test query at https://w.wiki/QQX
         SELECT ?item (REPLACE(STR(?item),STR(wd:),"") as ?qid)  
         ${languages.map(lang => '?itemLabel_' + lang).join(" ")} 
@@ -139,15 +169,15 @@ let translateQids = function (qids, languages) {
                 }
                 `;
 
+    // Formats the result into a dictionary for easy use     
 
-
-    let result = queryWikidata(sparql)
+    let result = queryWikidata(sparqlQuery)
         .then(result =>
-
             result.map(item => ({
                 [item.qid]: (function () {
                     var obj = {};
                     languages.forEach(lang => {
+
                         // Add the label for each language. 
                         // Run the label through the cleaning filter before using
                         if (item["itemLabel_" + lang]) {
@@ -160,11 +190,11 @@ let translateQids = function (qids, languages) {
                     return obj;
                 })()
             }))
-            )
+        )
         .then(mapped => Object.assign({}, ...mapped))
         .catch((error) => {
             console.log(error)
-          });
+        });
 
     return result;
 }
@@ -172,24 +202,28 @@ let translateQids = function (qids, languages) {
 //
 // Fetch JSON results from Wikidata using a SPARQL query
 //
+
 let queryWikidata = function (sparql) {
 
     let sparqlEndpoint = 'https://query.wikidata.org/sparql'
     let prettyResult = true
 
-    var resultPromise = fetch(
-            `${sparqlEndpoint}?query=${encodeURIComponent(
+    var resultPromise = limiter.schedule(() =>
+            fetch(
+                `${sparqlEndpoint}?query=${encodeURIComponent(
                             sparql
                             )}&format=json`, {
-                headers: {
-                    accept: "application/sparql-results+json"
+                    headers: {
+                        accept: "application/sparql-results+json",
+                        "user-agent": "join-osm-wikidata-label/0.1 (https://github.com/osm-in/indic-map/issues/1; arun.planemad@gmail.com) node-fetch" // See https://meta.wikimedia.org/wiki/User-Agent_policy
+                    }
                 }
-            }
-        )
+            ))
         .then(response => response.json())
         .then(json => {
 
             // Remap result values into a pretty object
+            
             let result = json.results.bindings;
 
             if (prettyResult) {
@@ -201,20 +235,27 @@ let queryWikidata = function (sparql) {
             }
 
             return result;
+        })
+        .catch(function (error) {
+            console.log("Network Error", error);
         });
 
     return resultPromise;
 }
 
-let result = joinWikidataLabels(features,
-    languages
-)
+
 
 // Wikidata label cleaning filter
-function cleanLabel(label){
+function cleanLabel(label) {
 
     // Remove text after comma. This removes common qualifier text eg. `Bhopal, Madhya Pradesh' -> 'Bhopal'
     label = label.split(',')[0];
 
     return label;
 }
+
+
+let result = joinWikidataLabels(features,
+    languages
+)
+
